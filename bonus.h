782d7caede96c0dbde23050a3a4887309ceb265c
WANG Yucheng <107743952+WANGAndyYucheng@users.noreply.github.com>
2022-10-25 23:13:48 +0800

    Add files via upload

---
+++ bonus.h
@@ -0,0 +1,41 @@
+#ifndef DEFORMER_H
+#define DEFORMER_H
+#include <Eigen/Dense>
+#include <Eigen/Sparse>
+#include "mesh.h"
+
+// Deform mesh using Laplacian coordinates
+class Deformer {
+public:
+	Deformer();
+	~Deformer();
+
+	void setMesh(Mesh* mesh);
+
+	/*====== Programming Assignment 2 ======*/
+	// This is the place where the editing techniques take place
+	void deform();
+	/*====== Programming Assignment 2 ======*/
+
+private:
+	/*====== Programming Assignment 2 ======*/
+	// Build left hand side matrix and pre-factorize it
+	void buildSystemMat();
+	/*====== Programming Assignment 2 ======*/
+
+	void clear();
+
+	int n_inter = 1;
+	double Wl0 = 0;
+	Mesh* mMesh;
+	std::vector< double > ori_area;
+	std::vector< double > curr_area;
+	std::vector< Vertex* > mRoiList;
+    // create mA and mB for deformer
+    Eigen::SparseMatrix<double> mA;
+    Eigen::MatrixX3d mB;
+	// Solver for pre-factorizing the system matrix of the deformation
+	Eigen::SimplicialLDLT< Eigen::SparseMatrix< double > >* mCholeskySolver;
+};
+
+#endif // DEFORMER_H
